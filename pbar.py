28034f0410c671777f209a6faedbb9954ba62afb
Ludwig Schmidt-Hackenberg <ludwig@schmidt-hackenberg.net>
2014-02-01 22:32:37 +0100

    refactoring for cleaner code

--- pbar.py
+++ pbar.py
@@ -22,8 +22,8 @@
 
     code taken from here: http://stackoverflow.com/questions/3173320/text-progress-bar-in-the-console?lq=1
     and here: http://pypi.python.org/pypi/progressbar/2.2
-    
-    get the newest version here: https://gist.github.com/shackenberg/3716039
+
+    get the newest version here: https://github.com/shackenberg/pbar.py
     by Ludwig Schmidt-Hackenberg
 """
 import sys
@@ -34,58 +34,97 @@ import os
  
 class ProgressBar(object):
 
-    def __init__(self, maxval, title=None):
-        self.maxval = maxval - 1
+    def __init__(self, max_value, title=None, start_state=-1, max_refeshrate=0.1):
+        self.max_value = self.adjust_max_value_for_0_indexing(max_value)
         self.start_time = time.time()
-        self.state = 0
-        try:
-            self.ncolumns = self.get_width_of_terminal()
-        except:
-            self.ncolumns = 80
-        if title is not None:
-            self.title = title + ' '
-        else:
-            self.title = ''
+        self.state = start_state
+        self.lenght = self.automatically_set_length_pbar()
+        self.title = self.prepare_title(title)
         self.time_last_update = 0
+        self.max_refreshrate = max_refeshrate
+        self.is_last_update = False
+
+    def adjust_max_value_for_0_indexing(self, max_value):
+        return max_value - 1
+
+    def automatically_set_length_pbar(self):
+        try:
+            return self.get_width_of_terminal()
+        except:
+            rule_of_thumb_standard_value = 80
+            return rule_of_thumb_standard_value
 
     def get_width_of_terminal(self):
         _, ncolumns = os.popen('stty size', 'r').read().split()
         return ncolumns
-    
-    def update(self, currentval=None):
-        if currentval is not None:
-            self.state = currentval
-        current_time = time.time()
-        complete_elapsed_time = current_time - self.start_time
-        complete_elapsed_time_pretty = str(timedelta(seconds=round(complete_elapsed_time)))
-        time_since_last_update = current_time - self.time_last_update
-        if (time_since_last_update > 1) | (self.state == self.maxval):
-            if (complete_elapsed_time > 3) & (self.state > 0):
-                estimated_time_left = complete_elapsed_time * (self.maxval / float(self.state) - 1)
-                estimated_time_left_pretty = str(timedelta(seconds=round(estimated_time_left)))
-                progress = int(round(self.state * 100.0 / self.maxval))
-                overhead = 25 + len(complete_elapsed_time_pretty) + len(self.title) + len(estimated_time_left_pretty)
-                max_bar_length = int(self.ncolumns) - overhead
-                bar_length = progress * max_bar_length / 100
-                output_string = '\r{4}[{0}] {1}% in {2} - {3} remaining'.format('#'*bar_length, progress,
-                                                                                complete_elapsed_time_pretty,
-                                                                                estimated_time_left_pretty,
-                                                                                self.title)
-                sys.stdout.write(output_string)
-            else:
-                progress = int(round(self.state * 100.0 / self.maxval))
-                overhead = 35 + len(complete_elapsed_time_pretty) + len(self.title)
-                max_bar_length = int(self.ncolumns) - overhead
-                bar_length = progress * max_bar_length / 100
-                output_string = '\r{3}[{0}] {1}% in {2}'.format('#'*bar_length, progress,
-                                                                complete_elapsed_time_pretty, self.title)
-                sys.stdout.write(output_string)
 
+    def prepare_title(self, title):
+        if title is None:
+            return ''
+        else:
+            return title + ' '
+
+    def update(self, current_value=None):
+        self.update_state(current_value)
+        current_time = time.time()
+        time_since_last_update = current_time - self.time_last_update
+        if self.state == self.max_value:
+            self.is_last_update = True
+        if (time_since_last_update > self.max_refreshrate) | self.is_last_update:
+            output_string = self.build_output_string(current_time)
+            self.print_pbar(output_string)
             self.time_last_update = current_time
-            sys.stdout.flush()
-        self.state += 1
-        if self.state == self.maxval + 1:
-            print
+        if self.is_last_update:
+            self.jump_to_newline()
+
+    def update_state(self, current_value):
+        if current_value is not None:
+            self.state = current_value
+        else:
+            self.state += 1
+
+    def time_div_to_short_str(self, time_div):
+        return str(timedelta(seconds=round(time_div)))
+
+    def compute_bar_length(self, overhead, progress):
+        max_bar_length = int(self.lenght) - overhead
+        return progress * max_bar_length / 100
+
+    def computed_estimate_time_left_pretty(self, complete_elapsed_time):
+        estimated_time_left = complete_elapsed_time * (self.max_value / float(self.state) - 1)
+        estimated_time_left_pretty = self.time_div_to_short_str(estimated_time_left)
+        return estimated_time_left_pretty
+
+    def build_output_string(self, current_time, show_estimated_time_left=False):
+        progress = int(round(self.state * 100.0 / self.max_value))
+        complete_elapsed_time = current_time - self.start_time
+        complete_elapsed_time_pretty = self.time_div_to_short_str(complete_elapsed_time)
+        if (complete_elapsed_time > 3) & (self.state > 0):
+            show_estimated_time_left = True
+
+        if show_estimated_time_left:
+                estimated_time_left_pretty = self.computed_estimate_time_left_pretty(complete_elapsed_time)
+                overhead = 25 + len(complete_elapsed_time_pretty) + len(self.title) + len(estimated_time_left_pretty)
+                bar_length = self.compute_bar_length(overhead, progress)
+                output_string = '\r{3}[{0}] {1}% in {2} - {4} remaining'.format('#'*bar_length, progress,
+                                                                                complete_elapsed_time_pretty,
+                                                                                self.title,
+                                                                                estimated_time_left_pretty)
+        else:
+                overhead = 35 + len(complete_elapsed_time_pretty) + len(self.title)
+                bar_length = self.compute_bar_length(overhead, progress)
+                output_string = '\r{3}[{0}] {1}% in {2}'.format('#'*bar_length, progress,
+                                                                complete_elapsed_time_pretty,
+                                                                self.title)
+
+        return output_string
+
+    def print_pbar(self, output_string):
+        sys.stdout.write(output_string)
+        sys.stdout.flush()
+
+    def jump_to_newline(self):
+        print
 
 if __name__ == '__main__':
     ## usage example
@@ -94,11 +133,18 @@ if __name__ == '__main__':
     example_title_example_one =  'explicit update'
     progressbar = ProgressBar(example_rangevalue, example_title_example_one)
     for new_value in range(example_rangevalue):
-        sleep(0.1)
-        progressbar.update(new_value)    
-    
+        time.sleep(0.1)
+        progressbar.update(new_value)
+
     example_title_example_two = 'implicit update by 1'
     progressbar = ProgressBar(example_rangevalue, example_title_example_two)
     for i in range(example_rangevalue):
-        sleep(0.1)
+        time.sleep(0.1)
+        progressbar.update()
+
+    example_title_example_two = 'slow updates'
+    example_rangevalue = 4
+    progressbar = ProgressBar(example_rangevalue, example_title_example_two)
+    for i in range(example_rangevalue):
+        time.sleep(3.8)
         progressbar.update()
